28dec2113f4c9ce67daadca266b03d5096074454
Bantree96 <51081373+Bantree96@users.noreply.github.com>
2022-11-16 19:52:34 +0900

    책임체인, 커맨드, 인터프리터 패턴 추가

---
+++ ChainOfResponsibility/Program.cs
@@ -0,0 +1,64 @@
+﻿abstract class ALogger
+{
+    protected ALogger Next { get; private set; }
+    
+    // 다음 포인터 Logger 설정
+    public ALogger SetNextLogger(ALogger logger)
+    {
+        // Property에 인스턴스 설정
+        Next = logger;
+        // 인스턴스 return
+        return logger;
+    }
+
+    // 다음 인스턴스에 write함수 호출
+    public virtual void Write(string data)
+    {
+        if(Next != null)
+        {
+            Next.Write(data);
+        }
+    }
+}
+
+class ConsoleLogger : ALogger
+{
+    public override void Write(string data)
+    {
+        Console.WriteLine($"ConsoleLogger - {data}");
+        // 다음 포인터 인스턴스의 함수 호출
+        base.Write(data);
+    }
+}
+
+class FileLogger : ALogger
+{
+    public override void Write(string data)
+    {
+        Console.WriteLine($"FileLogger - {data}");
+        // 다음 포인터 인스턴스의 함수 호출
+        base.Write(data);
+    }
+}
+
+class MailLogger : ALogger
+{
+    public override void Write(string data)
+    {
+        Console.WriteLine($"MailLogger - {data}");
+        // 다음 포인터 인스턴스의 함수 호출
+        base.Write(data);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var logger = new ConsoleLogger();
+        logger.SetNextLogger(new FileLogger())
+              .SetNextLogger(new MailLogger());
+
+        logger.Write("Hello World");
+    }
+}
---
+++ Command/Program.cs
@@ -0,0 +1,128 @@
+﻿class Car
+{
+    public void LeftTurn()
+    {
+        Console.WriteLine("LeftTurn!");
+    }
+    public void RightTurn()
+    {
+        Console.WriteLine("RightTurn!");
+    }
+    public void Break()
+    {
+        Console.WriteLine("Break!");
+    }
+    public void Accelerator()
+    {
+        Console.WriteLine("Accelerator!");
+    }
+}
+
+abstract class ACommand
+{
+    private Car car = new Car();
+    public ACommand(Car car)
+    {
+        // 멤버 변수 설정
+        this.car = car;
+    }
+
+    // 실행
+    public void Execute()
+    {
+        // 추상 함수 실행
+        Run(car);
+    }
+    // 추상 함수
+    protected abstract void Run(Car car);
+}
+
+class LeftTurnCommand : ACommand
+{
+    public LeftTurnCommand(Car car) : base(car){ }
+    protected override void Run(Car car)
+    {
+        car.LeftTurn();
+    }
+}
+
+
+class RightTurnCommand : ACommand
+{
+    public RightTurnCommand(Car car) : base(car) { }
+
+    protected override void Run(Car car)
+    {
+        car.RightTurn();
+    }
+}
+
+class AcceleratorCommand : ACommand
+{
+    public AcceleratorCommand(Car car) : base(car){ }
+    protected override void Run(Car car)
+    {
+        car.Accelerator();
+    }
+}
+
+class BreakCommand : ACommand
+{
+    public BreakCommand(Car car) : base (car){ }
+    protected override void Run(Car car)
+    {
+        car.Break();
+
+    }
+}
+
+
+class Driving
+{
+    // Command List
+    private List<ACommand> cmds = new List<ACommand>();
+    // 브레이크 커맨드 멤버 변수
+    private ACommand breakCmd;
+
+    // 생성자
+    public Driving(ACommand breakCmd)
+    {
+        this.breakCmd = breakCmd;
+    }
+
+    /// <summary>
+    /// 커맨드 추가 함수
+    /// </summary>
+    /// <param name="cmd"></param>
+    public void AddCommand(ACommand cmd)
+    {
+        cmds.Add(cmd);
+    }
+    
+    /// <summary>
+    /// 출발
+    /// </summary>
+    public void Start()
+    {
+        foreach (var cmd in this.cmds)
+        {
+            cmd.Execute();
+            this.breakCmd.Execute();
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car car = new Car();
+        Driving driving = new Driving(new BreakCommand(car));
+        driving.AddCommand(new AcceleratorCommand(car));
+        driving.AddCommand(new LeftTurnCommand(car));
+        driving.AddCommand(new RightTurnCommand(car));
+
+        // 출발
+        driving.Start();
+    }
+}
