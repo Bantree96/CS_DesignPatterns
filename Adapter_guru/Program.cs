a6ebe7e309477c3aeee41592faef1edcfd4f0ef6
태영 박 <typark@crevis.co.kr>
2022-12-16 10:38:22 +0100

    어댑터 패턴 기초 추가

---
+++ Adapter_guru/Program.cs
@@ -0,0 +1,35 @@
+﻿public interface ITarget
+{ 
+    string GetRequest();
+}
+
+class Adaptee
+{
+    public string GetSpecificRequest() { return "특정한 요청.";  }
+}
+
+class Adapter : ITarget
+{
+    private readonly Adaptee _adaptee;
+
+    public Adapter(Adaptee adaptee)
+    {
+        _adaptee = adaptee;
+    }
+
+    public string GetRequest()
+    {
+        return $"이것은 '{_adaptee.GetSpecificRequest()}' 입니다.";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Adaptee adaptee = new Adaptee();
+        ITarget target = new Adapter(adaptee);  
+
+        Console.WriteLine(target.GetRequest());
+    }
+}
